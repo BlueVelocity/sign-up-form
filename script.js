eb47fc87534aff5756ef6f703affd343c4862d35
BlueVelocity <josephrodney@hotmail.com>
2023-10-03 16:58:23 -0400

    Improve password validation

---
+++ script.js
@@ -0,0 +1,20 @@
+const passwordObj = document.querySelector('#password');
+const confirmPasswordObj = document.querySelector('#confirm-password');
+const noMatchIndicator = document.querySelector('span')
+
+passwordObj.addEventListener("keyup", checkIfMatching);
+confirmPasswordObj.addEventListener("keyup", checkIfMatching);
+
+passwordObj.setCustomValidity("Invalid field.")
+
+function checkIfMatching() {
+    let pass = passwordObj.value;
+    let compPass = confirmPasswordObj.value;
+    if(pass != compPass || pass === '' || pass.length < 8) {
+        passwordObj.setCustomValidity("Invalid field.")
+        confirmPasswordObj.setCustomValidity("Invalid field.")
+    } else {
+        passwordObj.setCustomValidity("")
+        confirmPasswordObj.setCustomValidity("")
+    }
+}
